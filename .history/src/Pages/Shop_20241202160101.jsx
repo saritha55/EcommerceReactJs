00de67448842d7ab32334b8e8c2716617f4ad959
Kumar <ratnakumarbt@gmail.com>
2024-12-02 16:16:41 -0500

    Added offer banner

--- .history/src/Pages/Shop_20241202160101.jsx
+++ .history/src/Pages/Shop_20241202160101.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import Hero from '../Components/Hero/Hero'
 import Popular from '../Components/Popular/Popular'
-import Offers from '../Components/Offers/Offers'
 
 const Shop = () => {
   return (
