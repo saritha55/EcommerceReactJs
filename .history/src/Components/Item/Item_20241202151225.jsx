f34391789f21ff57cdcdfb869fb711375a1faf66
Kumar <ratnakumarbt@gmail.com>
2024-12-02 15:38:49 -0500

    new changes in item,popular

---
+++ .history/src/Components/Item/Item_20241202151225.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Item.css'
+
+const Item = () => {
+  return (
+    <div className='item'>
+
+      
+    </div>
+  )
+}
+
+export default Item
---
+++ .history/src/Components/Popular/Popular_20241202151819.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Popular.css'
+const Popular = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Popular
